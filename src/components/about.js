803891a533a83250a293201ce1a46e120a4c240d
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-09-10 16:27:57 -0600

    Added structure and styles to about page

--- src/components/about.js
+++ src/components/about.js
@@ -4,14 +4,34 @@ import Navbar from './navbar';
 export default class About extends Component {
     render() {
         return (
-            <div>
+            <div className="about-wrapper">
                 <Navbar/>
-                <h1>This is the about page</h1>
-                <p>Emily's Aprons makes high quality Aprons out of only the best fabrics and materials. Choose us for perfect design, look, and functionality.</p>
-                <h1>Contact Us</h1>
-                <p>Phone: 800-555-3518</p>
-                <p>Email: emilysaprons@gmail.com</p>
-                <p>Fax: 888-555-1681</p>
+                <div className="info">
+                    <div className="information">
+                        <h1>About us</h1>
+                        <p>Emily's Aprons makes high quality Aprons out of only the best fabrics and materials. Choose us for perfect design, look, and functionality.</p>
+                        <h1>Contact Us</h1>
+                        <p>Phone: 800-555-3518</p>
+                        <p>Email: emilysaprons@gmail.com</p>
+                        <p>Fax: 888-555-1681</p>
+                        <h1>Love your apron? Leave us a review!</h1>
+                        <div className="about-icons">
+                            <div className="insta">
+                                <i className="fab fa-instagram"></i>
+                            </div>
+                            <div className="facebook">
+                                <i className="fab fa-facebook"></i>
+                            </div>
+                            <div className="snap">
+                                <i className="fab fa-snapchat"></i>
+                            </div>
+                            <div className="twitter">
+                                <i className="fab fa-twitter"></i>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -33,10 +33,10 @@ export class Dropdown extends Component {
         return (
             <div className="dropdown">
                 <div className="dropdown-content">
-                    <Link to="/browse" className="dropdown-link">Browse</Link>
-                    <Link to="/infantsBrowse" className="dropdown-link">Infants</Link>
-                    <Link to="/toddlersBrowse" className="dropdown-link">Toddlers</Link>
-                    <Link to="/kidsBrowse" className="dropdown-link">Kids</Link>
+                    <Link to="/browse" className="dropdown-links" id="browse-link">Browse</Link>
+                    <Link to="/infantsBrowse" className="dropdown-links">Infants</Link>
+                    <Link to="/toddlersBrowse" className="dropdown-links">Toddlers</Link>
+                    <Link to="/kidsBrowse" className="dropdown-links">Kids</Link>
                 </div>
             </div>
         );
